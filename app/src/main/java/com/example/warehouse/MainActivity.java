248c8d6d61d465e6c0c92e6ee5265e16a9ccbcb2
xie774505257@163.com <xiem992421810@>
2019-12-06 16:58:51 +0800

    tijiodaima

--- app/src/main/java/com/example/warehouse/MainActivity.java
+++ app/src/main/java/com/example/warehouse/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.warehouse;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("YYY","develop==============第一次提交的分支代码");
     }
 }
