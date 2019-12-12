ccc9a39fdea05287d67095ad3508a612f8223b6d
xie774505257@163.com <xiem992421810@>
2019-12-12 14:31:20 +0800

    tijiodaima

---
+++ app/src/main/java/com/example/warehouse/IconUtil.java
@@ -0,0 +1,12 @@
+package com.example.warehouse;
+
+import android.content.ComponentName;
+import android.content.pm.PackageManager;
+
+public class IconUtil {
+    /**
+     * @param useCode =1、为活动图标 =2 为用普通图标 =3、不启用判断
+     */
+
+
+}
