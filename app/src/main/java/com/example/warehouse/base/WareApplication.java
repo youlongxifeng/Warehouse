05b548d9b13648d65a69343a9fdd5a5cdab9b104
xie774505257@163.com <xiem992421810@>
2020-04-10 10:55:07 +0800

    提交代码

---
+++ app/src/main/java/com/example/warehouse/base/WareApplication.java
@@ -0,0 +1,11 @@
+package com.example.warehouse.base;
+
+import android.app.Application;
+
+public class WareApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
